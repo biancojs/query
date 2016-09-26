dbcdfbf4c335b332cc819ca9555bad2c9f3d4a80
Gianluca Guarini <gianluca.guarini@gmail.com>
2016-09-26 20:35:50 +0200

    updated: doc

--- index.next.js
+++ index.next.js
@@ -4,7 +4,7 @@ import domToArray from 'bianco.dom-to-array'
  * Simple helper to find DOM nodes returning them as array like loopable object
  * @param   { String|DOMNodeList } selector - either the query or the DOM nodes to arraify
  * @param   { HTMLElement }        ctx      - context defining where the query will search for the DOM nodes
- * @returns { Object } DOM nodes in an array like object
+ * @returns { Array } DOM nodes found in an array
  */
 export default function $(selector, ctx) {
   return domToArray(typeof selector === 'string' ?
