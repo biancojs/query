1d96d8ce18a5c43a9b1238f5dd517be30892398d
gianlucaguarini <gianluca.guarini@gmail.com>
2022-03-30 08:58:30 +0200

    fixed: types

--- index.d.ts
+++ index.d.ts
@@ -3,9 +3,9 @@ type Scope = Element
     | Document
 
 // borrowed from DOM querySelectorAll
-declare function $<K extends keyof HTMLElementTagNameMap>(selectors: K, scope?: Scope): NodeListOf<HTMLElementTagNameMap[K]>;
-declare function $<K extends keyof SVGElementTagNameMap>(selectors: K, scope?: Scope): NodeListOf<SVGElementTagNameMap[K]>;
-declare function $<E extends Element = Element>(selectors: string, scope?: Scope): NodeListOf<E>;
+declare function $<K extends keyof HTMLElementTagNameMap>(selectors: K, scope?: Scope): HTMLElementTagNameMap[K][];
+declare function $<K extends keyof SVGElementTagNameMap>(selectors: K, scope?: Scope): SVGElementTagNameMap[K][];
+declare function $<E extends Element = Element>(selectors: string, scope?: Scope): E[];
 declare function $<W extends Window>(selectors: W, scope?: Scope): [W];
 declare function $<D extends Document>(selectors: D, scope?: Scope): [D];
 
